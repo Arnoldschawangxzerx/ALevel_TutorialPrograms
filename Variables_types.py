e3e55dfee3559ae177304e9922261dbe13783c03
Mohammad Aaryan Hussain <141989940+Arnoldschawangxzerx@users.noreply.github.com>
2025-01-08 12:35:39 +0500

    Create Variables_types.py
    Added the integer variable initialization and operations performed

---
+++ Variables_types.py
@@ -0,0 +1,5 @@
+a=2        #intialize the 'a' variable storing an integer 2
+b=2        #intialize the 'b' variable storing an integer 2
+print("the sum of a and b: ",a+b)
+print("the integer stored in a variable is: " , a) #show the result 
+print("the integer stored in a variable is: " , b)
